e10919a3a20b3c048bf9a859ddecc95727e2559a
YoussefAhmedy <youssef.ahmedy189@gmail.com>
2025-08-13 17:54:00 +0300

    Create IProductService.cs

---
+++ Services/IProductService.cs
@@ -0,0 +1,10 @@
+using IBSRAStore.DTOs;
+
+namespace IBSRAStore.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+        Task<ProductDto?> GetProductByIdAsync(int id);
+    }
+}
